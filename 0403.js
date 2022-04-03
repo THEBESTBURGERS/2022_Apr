527ccf67e388d70a4bded1b3ed5483125643739b
chestnut1044 <wbk1620@gmail.com>
2022-04-04 00:38:00 +0900

    Learn : Prototype

---
+++ 0403.js
@@ -0,0 +1,76 @@
+// // 프로토타입이 null인 객체를 생성한다. 생성된 객체는 프로토타입 체인의 종점에 위치한다.
+// // obj → null
+// let obj = Object.create(null);
+// console.log(Object.getPrototypeOf(obj) === null); // true
+// // Object.prototype을 상속받지 못한다.
+// console.log(obj.toString()); // TypeError: obj.toString is not a function
+
+// // obj → Object.prototype → null
+// // obj = {};와 동일하다.
+// obj = Object.create(Object.prototype);
+// console.log(Object.getPrototypeOf(obj) === Object.prototype); // true
+
+// // obj → Object.prototype → null
+// // obj = { x: 1 };와 동일하다.
+// obj = Object.create(Object.prototype, {
+//   x: { value: 1, writable: true, enumerable: true, configurable: true }
+// });
+// // 위 코드는 다음과 동일하다.
+// // obj = Object.create(Object.prototype);
+// // obj.x = 1;
+// console.log(obj.x); // 1
+// console.log(Object.getPrototypeOf(obj) === Object.prototype); // true
+
+// const myProto = { x: 10 };
+// // 임의의 객체를 직접 상속받는다.
+// // obj → myProto → Object.prototype → null
+// obj = Object.create(myProto);
+// console.log(obj.x); // 10
+// console.log(Object.getPrototypeOf(obj) === myProto); // true
+
+// // 생성자 함수
+// function Person(name) {
+//   this.name = name;
+// }
+
+// // obj → Person.prototype → Object.prototype → null
+// // obj = new Person('Lee')와 동일하다.
+// obj = Object.create(Person.prototype);
+// obj.name = 'Lee';
+// console.log(obj.name); // Lee
+// console.log(Object.getPrototypeOf(obj) === Person.prototype); // true
+
+
+
+// 생성자 함수
+function Person(name) {
+  this.name = name;
+}
+
+// 프로토타입 메서드
+Person.prototype.sayHello = function () {
+  console.log(`Hi! My name is ${this.name}`);
+};
+
+// 정적 프로퍼티
+Person.staticProp = 'static prop';
+
+// 정적 메서드
+Person.staticMethod = function () {
+  console.log('staticMethod');
+};
+
+const me = new Person('Lee');
+
+// 생성자 함수에 추가한 정적 프로퍼티/메서드는 생성자 함수로 참조/호출한다.
+Person.staticMethod(); // staticMethod
+
+// 정적 프로퍼티/메서드는 생성자 함수가 생성한 인스턴스로 참조/호출할 수 없다.
+// 인스턴스로 참조/호출할 수 있는 프로퍼티/메서드는 프로토타입 체인 상에 존재해야 한다.
+// me.staticMethod(); // TypeError: me.staticMethod is not a function
+
+const test = new Person('Hwang');
+
+console.log(test.staticProp);
+console.log(test)
+//test.staticMethod();
