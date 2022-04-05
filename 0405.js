1a4e860519a668b6b37b91b482dead3810ffae24
chestnut1044 <wbk1620@gmail.com>
2022-04-05 20:19:19 +0900

    Learn : Built-in

---
+++ 0405.js
@@ -0,0 +1,196 @@
+// 전역 프로퍼티는 window를 생략하고 참조할 수 있다.
+console.log(window.Infinity === Infinity); // true
+
+// 양의 무한대
+console.log(3/0);  // Infinity
+// 음의 무한대
+console.log(-3/0); // -Infinity
+// Infinity는 숫자값이다.
+console.log(typeof Infinity); // number
+
+
+console.log(window.NaN); // NaN
+
+console.log(Number('xyz')); // NaN
+console.log(1 * 'string');  // NaN
+console.log(typeof NaN);    // number
+
+
+console.log(window.undefined); // undefined
+
+var foo;
+console.log(foo); // undefined
+console.log(typeof undefined); // undefined
+
+
+// 표현식인 문
+eval('1 + 2;'); // -> 3
+// 표현식이 아닌 문
+eval('var x = 5;'); // -> undefined
+
+// eval 함수에 의해 런타임에 변수 선언문이 실행되어 x 변수가 선언되었다.
+console.log(x); // 5
+
+// 객체 리터럴은 반드시 괄호로 둘러싼다.
+const o = eval('({ a: 1 })');
+console.log(o); // {a: 1}
+
+// 함수 리터럴은 반드시 괄호로 둘러싼다.
+const f = eval('(function() { return 1; })');
+console.log(f()); // 1
+
+// 인수가 유한수이면 true를 반환한다.
+isFinite(0);    // -> true
+isFinite(2e64); // -> true
+isFinite('10'); // -> true: '10' → 10
+isFinite(null); // -> true: null → 0
+
+// 인수가 무한수 또는 NaN으로 평가되는 값이라면 false를 반환한다.
+isFinite(Infinity);  // -> false
+isFinite(-Infinity); // -> false
+
+// 인수가 NaN으로 평가되는 값이라면 false를 반환한다.
+isFinite(NaN);     // -> false
+isFinite('Hello'); // -> false
+isFinite('2005/12/12'); // -> false
+
+
+
+// 숫자
+isNaN(NaN); // -> true
+isNaN(10);  // -> false
+
+// 문자열
+isNaN('blabla'); // -> true: 'blabla' => NaN
+isNaN('10');     // -> false: '10' => 10
+isNaN('10.12');  // -> false: '10.12' => 10.12
+isNaN('');       // -> false: '' => 0
+isNaN(' ');      // -> false: ' ' => 0
+
+// 불리언
+isNaN(true); // -> false: true → 1
+isNaN(null); // -> false: null → 0
+
+// undefined
+isNaN(undefined); // -> true: undefined => NaN
+
+// 객체
+isNaN({}); // -> true: {} => NaN
+
+// date
+isNaN(new Date());            // -> false: new Date() => Number
+isNaN(new Date().toString()); // -> true:  String => NaN
+
+// 문자열을 실수로 해석하여 반환한다.
+parseFloat('3.14');  // -> 3.14
+parseFloat('10.00'); // -> 10
+
+// 공백으로 구분된 문자열은 첫 번째 문자열만 변환한다.
+parseFloat('34 45 66'); // -> 34
+parseFloat('40 years'); // -> 40
+
+// 첫 번째 문자열을 숫자로 변환할 수 없다면 NaN을 반환한다.
+parseFloat('He was 40'); // -> NaN
+
+// 앞뒤 공백은 무시된다.
+parseFloat(' 60 '); // -> 60
+
+
+// 문자열을 정수로 해석하여 반환한다.
+parseInt('10');     // -> 10
+parseInt('10.123'); // -> 10
+
+// 10'을 10진수로 해석하고 그 결과를 10진수 정수로 반환한다
+parseInt('10'); // -> 10
+// '10'을 2진수로 해석하고 그 결과를 10진수 정수로 반환한다
+parseInt('10', 2); // -> 2
+// '10'을 8진수로 해석하고 그 결과를 10진수 정수로 반환한다
+parseInt('10', 8); // -> 8
+// '10'을 16진수로 해석하고 그 결과를 10진수 정수로 반환한다
+parseInt('10', 16); // -> 16
+
+
+const x = 15;
+
+// 10진수 15를 2진수로 변환하여 그 결과를 문자열로 반환한다.
+x.toString(2); // -> '1111'
+// 문자열 '1111'을 2진수로 해석하고 그 결과를 10진수 정수로 반환한다
+parseInt(x.toString(2), 2); // -> 15
+
+// 10진수 15를 8진수로 변환하여 그 결과를 문자열로 반환한다.
+x.toString(8); // -> '17'
+// 문자열 '17'을 8진수로 해석하고 그 결과를 10진수 정수로 반환한다
+parseInt(x.toString(8), 8); // -> 15
+
+// 10진수 15를 16진수로 변환하여 그 결과를 문자열로 반환한다.
+x.toString(16); // -> 'f'
+// 문자열 'f'를 16진수로 해석하고 그 결과를 10진수 정수로 반환한다
+parseInt(x.toString(8), 8); // -> 15
+
+// 숫자값을 문자열로 변환한다.
+x.toString(); // -> '15'
+// 문자열 '15'를 10진수로 해석하고 그 결과를 10진수 정수로 반환한다
+parseInt(x.toString()); // -> 15
+
+
+// 완전한 URI
+const uri = 'http://example.com?name=이웅모&job=programmer&teacher';
+
+// encodeURI 함수는 완전한 URI를 전달받아 이스케이프 처리를 위해 인코딩한다.
+const enc = encodeURI(uri);
+console.log(enc);
+// http://example.com?name=%EC%9D%B4%EC%9B%85%EB%AA%A8&job=programmer&teacher
+
+
+const uri = 'http://example.com?name=이웅모&job=programmer&teacher';
+
+// encodeURI 함수는 완전한 URI를 전달받아 이스케이프 처리를 위해 인코딩한다.
+const enc = encodeURI(uri);
+console.log(enc);
+// http://example.com?name=%EC%9D%B4%EC%9B%85%EB%AA%A8&job=programmer&teacher
+
+// decodeURI 함수는 인코딩된 완전한 URI를 전달받아 이스케이프 처리 이전으로 디코딩한다.
+const dec = decodeURI(enc);
+console.log(dec);
+// http://example.com?name=이웅모&job=programmer&teacher
+
+
+// URI의 쿼리 스트링
+const uriComp = 'name=이웅모&job=programmer&teacher';
+
+// encodeURIComponent 함수는 인수로 전달받은 문자열을 URI의 구성요소인 쿼리 스트링의 일부로 간주한다.
+// 따라서 쿼리 스트링 구분자로 사용되는 =, ?, &까지 인코딩한다.
+let enc = encodeURIComponent(uriComp);
+console.log(enc);
+// name%3D%EC%9D%B4%EC%9B%85%EB%AA%A8%26job%3Dprogrammer%26teacher
+
+let dec = decodeURIComponent(enc);
+console.log(dec);
+// 이웅모&job=programmer&teacher
+
+// encodeURI 함수는 인수로 전달받은 문자열을 완전한 URI로 간주한다.
+// 따라서 쿼리 스트링 구분자로 사용되는 =, ?, &를 인코딩하지 않는다.
+enc = encodeURI(uriComp);
+console.log(enc);
+// name=%EC%9D%B4%EC%9B%85%EB%AA%A8&job=programmer&teacher
+
+dec = decodeURI(enc);
+console.log(dec);
+// name=이웅모&job=programmer&teacher
+
+// 전역 변수 x는 호이스팅이 발생한다.
+console.log(x); // undefined
+// 전역 변수가 아니라 단지 전역 객체의 프로퍼티인 y는 호이스팅이 발생하지 않는다.
+console.log(y); // ReferenceError: y is not defined
+
+var x = 10; // 전역 변수
+
+function foo () {
+  // 선언하지 않은 식별자에 값을 할당
+  y = 20; // window.y = 20;
+}
+foo();
+
+// 선언하지 않은 식별자 y를 전역에서 참조할 수 있다.
+console.log(x + y); // 30
+
