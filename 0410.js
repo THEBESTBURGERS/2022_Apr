959ce504911e6b195e5efef9b3c90711be152f59
chestnut1044 <wbk1620@gmail.com>
2022-04-10 20:19:19 +0900

    Learn : Prototype

---
+++ 0410.js
@@ -0,0 +1,34 @@
+// // 전역 변수 선언
+// const x = 1;
+// const y = 2;
+
+// // 함수 정의
+// function foo(a) {
+//   // 지역 변수 선언
+//   const x = 10;
+//   const y = 20;
+
+//   // 메서드 호출
+//   console.log(a + x + y); // 130
+// }
+
+// // 함수 호출
+// foo(100);
+
+// // 메서드 호출
+// console.log(x + y); // 3
+
+
+const x = 1;
+
+function foo () {
+  const y = 2;
+
+  function bar () {
+    const z = 3;
+    console.log(x + y + z);
+  }
+  bar();
+}
+
+foo(); // 6
