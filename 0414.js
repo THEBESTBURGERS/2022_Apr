21be48c6a0c6c4ddbc70e2b06ac294eb9a894e1b
chestnut1044 <wbk1620@gmail.com>
2022-04-14 23:55:59 +0900

    Learn : Closure

---
+++ 0414.js
@@ -0,0 +1,27 @@
+// const x = 1;
+
+// function outerFunc() {
+//   const x = 10;
+
+//   function innerFunc() {
+//     console.log(x); // 10
+//   }
+
+//   innerFunc();
+// }
+
+// outerFunc();
+
+const x = 1;
+
+function foo() {
+  const x = 10;
+  bar();
+}
+
+function bar() {
+  console.log(x);
+}
+
+foo(); // ?
+bar(); // ?
