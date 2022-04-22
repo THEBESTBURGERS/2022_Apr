2e27444247836747ed2a3eb0b23aa9df6ac724f1
chestnut1044 <wbk1620@gmail.com>
2022-04-22 21:35:16 +0900

    Learn : Class

---
+++ 0422.js
@@ -0,0 +1,21 @@
+// const Person = '';
+
+// {
+//   // 호이스팅이 발생하지 않는다면 ''이 출력되어야 한다.
+//   console.log(Person);
+//   // ReferenceError: Cannot access 'Person' before initialization
+
+//   // 클래스 선언문
+//   class Person {}
+// }
+
+
+const Person = class MyClass {};
+
+// 함수 표현식과 마찬가지로 클래스를 가리키는 식별자로 인스턴스를 생성해야 한다.
+const me = new Person();
+
+// 클래스 이름 MyClass는 함수와 동일하게 클래스 몸체 내부에서만 유효한 식별자다.
+// console.log(MyClass); // ReferenceError: MyClass is not defined
+
+// const you = new MyClass(); // ReferenceError: MyClass is not defined
